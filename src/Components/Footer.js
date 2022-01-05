e4efc70fc853bec7ac933bf5773ec25e95cfabd9
aizadi <armanizadi98@gmail.com>
2022-01-05 18:51:08 -0500

    Update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Arman Izadi</li>
-              <li>Design by <a title="Tim Baker" href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
+              {/*<li>Design by <a title="Tim Baker" href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>*/}
            </ul>
 
         </div>
